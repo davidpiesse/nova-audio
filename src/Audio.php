2ba885852d5340e159420cf36e79c42b01de3921
Jason Lavorante <jason@curiousmedia.com>
2018-09-26 09:33:36 -0600

    Move default preview callback into constructor to mirror the file field.

--- src/Audio.php
+++ src/Audio.php
@@ -18,14 +18,14 @@ class Audio extends File
 
     public $showOnIndex = true;
 
-    public function preview(callable $previewUrlCallback)
+    public function __construct(string $name, ?string $attribute = null, ?string $disk = 'public', ?callable $storageCallback = null)
     {
-        $this->previewUrlCallback = function(){
+        parent::__construct($name, $attribute, $disk, $storageCallback);
+
+        $this->preview(function() {
             return $this->value
                 ? Storage::disk($this->disk)->url($this->value)
                 : null;
-        };
-
-        return $this;
+        });
     }
 }
